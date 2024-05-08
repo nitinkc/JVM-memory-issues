82a6562745ae9d096deee15051d5ce2a7e420b46
Nitin Chaurasia <gs.nitin@gmail.com>
2024-05-07 19:24:19 -0600

    memory issues

---
+++ src/main/noPackage/CompilationDemo.java
@@ -0,0 +1,19 @@
+public class CompilationDemo {
+
+    //
+    // java -XX:+TieredCompilation -XX:+PrintCompilation CompilationDemo
+
+    public static int compute(int x) {
+        return x * x;
+    }
+
+    // A method that repeatedly calls the compute method
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000000; i++) {
+            //The repeated calls to the compute method simulate a scenario where certain methods become
+            // hotspots and are prime candidates for compilation
+            int compute = compute(i);
+            //System.out.println(compute);
+        }
+    }
+}
